45f1d586fa0235757dd863fa626217aa68beac29
unknown <pabrams@PJABRAM-CY84LS1.utsystem.local>
2016-01-08 11:53:37 -0600

    added some basic color related logic

--- full-stack-web-development-specialization/HTML CSS and JavaScript-homework2.js
+++ full-stack-web-development-specialization/HTML CSS and JavaScript-homework2.js
@@ -3,27 +3,30 @@ var colors = ['blue','yellow','cyan'];
 var colors_list = colors.join();
 console.log(colors_list);
 colors.sort();
+console.log("the indexOf value is " + colors.indexOf('jdj'))
 
 
 var target; 
-var guess_input_text;
 var guess_input;
-var finished = true;
+var finished = false;
 var guesses = 0;
 
 
 function do_game() {
 	var random_number = Math.random() * colors.length;
 	var random_number_integer = Math.floor(random_number);
-	target_index = random_number_integer + 1;
-	console.log(target_index);
-	console.log(random_number);
+	target = random_number_integer + 1;
+	console.log("the original random number is "+ random_number);
+	console.log("target index is "+ target);
+	console.log("the color would be " + colors[target - 1])
+	
 	while (!finished) {
-		guess_input_text = prompt("i'm thinking of a number from 1 to 100");
-		guess_input = parseInt(guess_input_text);
+		guess_input = prompt("i'm thinking of one of these colors: \n\n" 
+			                        + colors_list 
+									+ "\n\n What color am I thinking of?");
 		guesses += 1;
-//		finished = check_guess();
-		if (!guess_input_text) {
+		finished = check_guess();
+		if (!guess_input) {
 			finished = false;
 		}
 	}
@@ -32,28 +35,32 @@ function do_game() {
 do_game();
 
 function check_guess() {
-	if (isNaN(guess_input)) {
-		alert("please enter a number");
-		return false;
-	}
 	
-	if ( (guess_input<1 ) || (guess_input>100) ) {
-		alert("out of range");
+	if ( (guess_input) && colors.indexOf(guess_input) == -1) { // if not in the array
+		alert("Sorry I don't recognize that color\n\nPlease try again.");
 		return false;
 	};
 
-	if (guess_input > target) {
-		alert("too high");
+	if (guess_input > target) { // if guess is not correct
+		alert("Sorry your guess is not correct\n\n"
+			   + "Hint your color is alphabetically lower than mine\n\n"
+			   + "Please try again.");
 		return false;
 	}
 
 	if (guess_input < target) {
-		alert("too low");
+		alert("Sorry your guess is not correct\n\n"
+			   + "Hint your color is alphabetically higher than mine\n\n"
+			   + "Please try again.");
 		return false;
 	}
 
 	if (guess_input == target) {
-		alert("you got it in " + guesses + "attempts")
+		alert("Congratulations. You have guessed the color! \n\n"
+			   + "It took you " + guesses + "to finish the game.\n\n"
+			   + "You can see the color in the bacground");
+		body = document.getElementsByTagName('body');
+		body.color = target;		
 		return true;
 	}
  
