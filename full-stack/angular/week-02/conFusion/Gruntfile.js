0f9a9cfd00745ed67ff416803a5b69d4b362d8a4
PJ <github@mediaslug.com>
2016-02-27 16:47:04 -0600

    added Watch, Connect and Serve Tasks

--- full-stack/angular/week-02/conFusion/Gruntfile.js
+++ full-stack/angular/week-02/conFusion/Gruntfile.js
@@ -109,8 +109,57 @@ module.exports = function(grunt) {
 	            assetsDirs: ['dist', 'dist/styles']
 	        }
 	    },
+
+	    watch: {
+	        copy: {
+	            files: [ 'app/**', '!app/**/*.css', '!app/**/*.js'],
+	            tasks: [ 'build' ]
+	        },
+	        scripts: {
+	            files: ['app/scripts/app.js'],
+	            tasks:[ 'build']
+	        },
+	        styles: {
+	            files: ['app/styles/mystyles.css'],
+	            tasks:['build']
+	        },
+	        livereload: {
+	            options: {
+	                livereload: '<%= connect.options.livereload %>'
+	            },
+	            files: [
+	                'app/{,*/}*.html',
+	                '.tmp/styles/{,*/}*.css',
+	                'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+	            ]
+	      	}
+    	},
+
+    	connect: {
+	      options: {
+	        port: 9000,
+	        // Change this to '0.0.0.0' to access the server from outside.
+	        hostname: 'localhost',
+	        livereload: 35729
+	      },
+	      dist: {
+	        options: {
+	          open: true,
+	          base:{
+	               path: 'dist',
+	            options: {
+	                index: 'menu.html',
+	                maxAge: 300000
+	            }
+	          }
+	        }
+	      }
+	    },
+
 	});
 
 	grunt.registerTask('build', ['clean','jshint','useminPrepare','concat','cssmin','uglify','copy','filerev','usemin']);
 	grunt.registerTask('default',['build']);
+	grunt.registerTask('serve',['build','connect:dist','watch']);
+
 };
