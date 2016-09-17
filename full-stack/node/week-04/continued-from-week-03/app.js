5e498daf3dfb7f797287bcbfe6757c93eabad19d
PJ <github@mediaslug.com>
2016-09-17 14:43:40 -0500

    added loopback server. completed facebook auth example

--- full-stack/node/week-04/continued-from-week-03/app.js
+++ full-stack/node/week-04/continued-from-week-03/app.js
@@ -6,7 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var authenticate = require('./authenticate');
+var User = require('./models/user');
+
+
 
 // include the config file
 var config = require('./config');
@@ -51,14 +54,10 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
 app.use(cookieParser());
 
-// passport config
-var User = require('./models/user');
-app.use(passport.initialize());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
