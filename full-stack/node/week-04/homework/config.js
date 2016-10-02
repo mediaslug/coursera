c03a5f45b20f19fd8148403285ac718b44f7f601
PJ <github@mediaslug.com>
2016-10-02 00:53:47 -0500

    removed unused homework folder

--- full-stack/node/week-04/homework/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientID: '704556503016332',
-        clientSecret: '26f9b1de93439f3353a4e35f35664752',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
-    }
-}
